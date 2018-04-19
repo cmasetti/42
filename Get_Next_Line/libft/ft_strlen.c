8ed97fdd2ecd22eb67e3b917c7af411377c319c4
Camila Masetti <cmasetti@student.42.fr>
2018-04-19 15:03:40 +0200

    writes in ftstrlen

--- Get_Next_Line/libft/ft_strlen.c
+++ Get_Next_Line/libft/ft_strlen.c
@@ -6,20 +6,20 @@
 /*   By: cmasetti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 18:53:04 by cmasetti          #+#    #+#             */
-/*   Updated: 2018/04/12 15:02:38 by cmasetti         ###   ########.fr       */
+/*   Updated: 2018/04/19 14:52:24 by cmasetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
-#include <stdio.h> //to take off
 
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 	
-	printf("%s", s);//test
+	write(1, "a", 1);//test
 	i = 0;
 	while (s && s[i])
 		i++;
+	write(1, "b", 1);//test
 	return (i);
 }
