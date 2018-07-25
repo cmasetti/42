fab322e8a0fc06a613a12f723a94fd98a6a6e6b1
Camila Masetti <cmasetti@student.42.fr>
2018-07-25 20:14:31 +0200

    ecrit final corrections bug started

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cmasetti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 17:11:28 by cmasetti          #+#    #+#             */
-/*   Updated: 2018/07/22 13:44:43 by cmasetti         ###   ########.fr       */
+/*   Updated: 2018/07/25 20:13:39 by cmasetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 // resultat sans \n
 // 1 seule variable statique
 // gerer plusieurs descripteurs de fichier
+//ft_strdel ou free?
 
 int		charpos(char *str, char c)
 {
@@ -36,6 +37,58 @@ int		charpos(char *str, char c)
 	return (-1);
 }
 
+//function to copy a string after the z chracters and free the tmp variable
+
+char	*strzcpy(int z, char *str)
+{
+	char	*tmp;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(str);
+	if (len <= z)
+	{
+		tmp = NULL;
+		return (tmp);
+	}
+	tmp = ft_strnew(len - z);
+	while (z + i <= len)
+	{
+		tmp[i] = str[z + i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
+
+//function to join a string and beginning (n chars) of another string + free everything
+char	*strnjoin(char *s1, int n, char *s2)
+{
+	char	*newstr;
+	int		len;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s2)
+		return (s1);
+	if (!s1)
+	{
+		if ((newstr = ft_strnew(n)) == NULL)
+			return (NULL);
+		ft_strncpy(newstr, s2, n);
+	}
+	else
+	{
+		len = ft_strlen(s1) + n;
+		if ((newstr = ft_strnew(len)) == NULL)
+			return (NULL);
+		ft_strncpy(newstr, s1, ft_strlen(s1));
+		ft_strncat(newstr, s2, n);
+	}
+	return (newstr);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static char	*strpast;
@@ -46,32 +99,51 @@ int		get_next_line(const int fd, char **line)
 	if (strpast != 0 && charpos(strpast, '\n') >= 0)
 	{
 		*line = ft_strnew(charpos(strpast, '\n'));
-		ft_strncpy(*line, strpast, charpos(strpast, '\n'));
-		strpast = strpast(reste);
+		ft_strncpy(*line, strpast, charpos(strpast, '\n'));//ok
+		tmp = strzcpy(charpos(strpast, '\n') + 1, strpast);//errroor tmp semble vide
+	ft_putstr(*line);//
+	ft_putstr(strpast);//
+	ft_putstr("A");//
+		ft_strdel(&strpast);
+	ft_putstr("A");//
 		strpast = tmp;
+	ft_putstr("A");//
+	ft_putstr(strpast);//
+		free(tmp);
 		return (1);
 	}
 	n = read(fd, str, BUFF_SIZE); //si \n pas present dans strpast ou que strpast est nul
 	if (strpast == 0 && n == 0)
-		return (0);
+	{ft_putstr("B");//
+		return (0);}
 	if (n < 0)
-		return (-1);
+	{ft_putstr("C");//
+		return (-1);}
 	if (n == 0)
 	{
+	ft_putstr("D");//
 		*line = ft_strtrim(strpast);
-		ft_strdel(strpast);
+		ft_strclr(strpast);//plutot que free pour stopper la boucle infinie ?
 		return (1);
 	}
 	str[n] = '\0';
 	if (charpos(str, '\n') >= 0)
 	{
-		*line = join(strpast, str(debut));
-		strpast = str(reste);
+	ft_putstr("E");//
+		*line = strnjoin(strpast, charpos(str, '\n'), str);//ok
+		//free strpast and create new of good size
+		tmp = strzcpy(charpos(str, '\n') + 1, str);
+		free(strpast);
+		strpast = tmp;//ok
+		free(tmp);
 		return (1);
 	}
 	else
 	{
+	ft_putstr("F");//
 		strpast = ft_strjoin(strpast, str); //faire join special qui free strpast ancien et str
 		get_next_line(fd, line);
 	}
+	ft_putstr("Z");
+	return (0);// pour faire disparaitre error may reach end of non void function
 }
